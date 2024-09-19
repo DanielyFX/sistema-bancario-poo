e9a60773017583009312c958b96c8406adb3624a
Daniely de Faria Xavier <danixavier815@gmail.com>
2024-09-19 20:10:28 -0300

    Fazendo pacote com o código sistema-bancario

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+
+setup(
+    name="sistema-bancario-poo",
+    version="0.0.1",
+    author="Daniely de Faria Xavier",
+    author_email="danixavier815@gmail.com",
+    description="Processamento e comparação de similaridade entre imagens.",
+    long_description=page_description,   # tira do readme.md
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanielyFX/sistema-bancario-poo",
+    package=find_packages(),  #procura módulos e sub módulos
+    python_requires='>=3.8'
+)
